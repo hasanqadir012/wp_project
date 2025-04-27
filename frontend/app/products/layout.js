5a87850e56891b8e0867f7fe502c88ff67d76ab7
umarorakzai012 <umarorakzai012@gmail.com>
2025-04-27 18:04:23 +0500

    added tailwind and shadcn for easy components

---
+++ frontend/app/products/layout.js
@@ -0,0 +1,7 @@
+import "../globals.css";
+
+export default function RootLayout({ children }) {
+    return (
+        children
+    );
+}
--- frontend/app/products/page.js
+++ frontend/app/products/page.js
@@ -1,39 +1,21 @@
-"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-import { useEffect, useState } from 'react';
+export default async function Products(){
+  const productsJson = await fetch("http://localhost:5032/api/products", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 
-export default function Products() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch("http://localhost:5032/api/products", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-
-        const data = await response.json();
-        console.log(data);
-        
-        setProducts(data);
-      } catch (error) {
-        console.error('Error fetching products:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, []);
-
-  if (loading) return <p>Loading...</p>;
+  const products = await productsJson.json();
 
   return (
     <div>
       <h1>Products List</h1>
+      <Button>Button</Button>
+      <Input />
       <ul>
         {products.map((product) => (
           <li key={product.id}>{product.name} - ${product.price}</li>
